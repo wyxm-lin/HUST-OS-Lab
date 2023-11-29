dabf33ef11a4cf5efa8f3596929a80272d6eff4f
wyxm <3460437682@qq.com>
2023-11-29 23:17:40 +0800

    no compile error

--- work/main.c
+++ work/main.c
@@ -7,21 +7,21 @@ int flag = 0;
 int main() {
     flag = 1;
     int id = fork();
-    if (id == 0) {
-        wait(NULL);
-        printf("flag = %d\n", flag);
-    }
-    else {
+    if (id == 0) { // 子进程
         flag = 2;
         int iid = fork();
-        if (iid == 0) {
-            wait(NULL); 
-            printf("flag = %d\n", flag);
-        }
-        else {
+        if (iid == 0) { // 孙子进程
             flag = 3;
             printf("flag = %d\n", flag);
         }
+        else {
+            wait(NULL);
+            printf("flag = %d\n", flag);
+        }
+    }
+    else { // 父进程
+        wait(NULL);
+        printf("flag = %d\n", flag);
     }
     return 0;
 }
