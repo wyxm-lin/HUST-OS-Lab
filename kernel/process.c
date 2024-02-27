2538eb9663ddc526fbd6be75c4b4a6d4862e98be
wyxm <3460437682@qq.com>
2024-02-27 10:28:17 +0800

    merge

--- kernel/process.c
+++ kernel/process.c
@@ -203,6 +203,7 @@ int do_fork( process* parent)
             int index = (parent->user_heap.free_pages_address[i] - heap_bottom) / PGSIZE;
             free_block_filter[index] = 1;
           }
+        }
         {
           int free_block_filter[MAX_HEAP_PAGES];
           memset(free_block_filter, 0, MAX_HEAP_PAGES);
@@ -215,19 +216,19 @@ int do_fork( process* parent)
           // copy and map the heap blocks
           for (uint64 heap_block = current->user_heap.heap_bottom;
               heap_block < current->user_heap.heap_top; heap_block += PGSIZE) {
-            if (free_block_filter[(heap_block - heap_bottom) / PGSIZE])  // skip free blocks
-              continue;
-          // copy and map the heap blocks
-          for (uint64 heap_block = current->user_heap.heap_bottom;
-              heap_block < current->user_heap.heap_top; heap_block += PGSIZE) {
-            if (free_block_filter[(heap_block - heap_bottom) / PGSIZE])  // skip free blocks
-              continue;
+              if (free_block_filter[(heap_block - heap_bottom) / PGSIZE])  // skip free blocks
+                continue;
+            // copy and map the heap blocks
+            for (uint64 heap_block = current->user_heap.heap_bottom;
+                heap_block < current->user_heap.heap_top; heap_block += PGSIZE) {
+              if (free_block_filter[(heap_block - heap_bottom) / PGSIZE])  // skip free blocks
+                continue;
 
-            void* child_pa = alloc_page();
-            memcpy(child_pa, (void*)lookup_pa(parent->pagetable, heap_block), PGSIZE);
-            user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)child_pa,
-                        prot_to_type(PROT_WRITE | PROT_READ, 1));
-          }
+              void* child_pa = alloc_page();
+              memcpy(child_pa, (void*)lookup_pa(parent->pagetable, heap_block), PGSIZE);
+              user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)child_pa,
+                          prot_to_type(PROT_WRITE | PROT_READ, 1));
+            }
             void* child_pa = alloc_page();
             memcpy(child_pa, (void*)lookup_pa(parent->pagetable, heap_block), PGSIZE);
             user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)child_pa,
@@ -239,8 +240,8 @@ int do_fork( process* parent)
 
           // copy the heap manager from parent to child
           memcpy((void*)&child->user_heap, (void*)&parent->user_heap, sizeof(parent->user_heap));
-          break;
         }
+        break;
       case CODE_SEGMENT:
         // TODO (lab3_1): implment the mapping of child code segment to parent's
         // code segment.
@@ -260,10 +261,11 @@ int do_fork( process* parent)
         child->mapped_info[child->total_mapped_region].npages =
           parent->mapped_info[i].npages;
         child->mapped_info[child->total_mapped_region].seg_type = CODE_SEGMENT;
-        child->total_mapped_region++; // TODO: don't understand this line
+        child->total_mapped_region++;
         break;
     }
   }
+  
 
   child->status = READY;
   child->trapframe->regs.a0 = 0;
